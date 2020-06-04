ed639957c2931eae50c88e708f5cccd008d64602
JuanCruzGM99 <55814774+JuanCruzGM99@users.noreply.github.com>
2020-06-04 19:11:10 -0300

    Soluciojn creada MVC

---
+++ castelar.parcial1.gonzalezmoro.juancruz/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace castelar.parcial1.gonzalezmoro.juancruz
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ castelar.parcial1.gonzalezmoro.juancruz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="castelar.parcial1.gonzalezmoro.juancruz.MvcApplication" Language="C#" %>
