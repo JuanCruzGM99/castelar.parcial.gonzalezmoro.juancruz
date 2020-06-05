40f6626d725d0854564f6693135ebb1531fa098c
JuanCruzGM99 <55814774+JuanCruzGM99@users.noreply.github.com>
2020-06-04 21:49:09 -0300

    Logica JS agregada, punto

---
+++ castelar.parcial1.gonzalezmoro.juancruz/Scripts/Constelaciones.js
@@ -0,0 +1,17 @@
+﻿function Guardar()
+{
+    function Constelacion(siglo, nombre) {
+        this.Nombre = nombre;
+        this.Siglo = siglo;
+    }
+
+    var InputSiglo = document.getElementById("InputSiglo").value;
+    var InputNombre = document.getElementById("InputNombre").value;
+    if (InputNombre.length == 0 || InputNombre == null && InputSiglo.length == 0 || InputSiglo == null) {
+        alert("No ha llenado todos los campos solicitados");
+    }
+    else {
+        var Constelacion = new Constelacion(InputSiglo, InputNombre);
+        document.getElementById("table").innerHTML += '<tbody><tr><td> ' + Constelacion.Siglo + '</td><td>' + Constelacion.Nombre + '</td></tr></tbody >';
+    }
+}
